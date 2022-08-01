526331e3dcf8a8f468d2940ede6af9a3839c3cf4
Misty94 <mms.strickland@gmail.com>
2022-07-31 23:28:26 -0400

    java fundamentals

---
+++ Strickland_Misty_First_Java_Program/Misty.java
@@ -0,0 +1,7 @@
+public class Misty {
+    public static void main(String[] args) {
+        System.out.println("My name is Misty Strickland.");
+        System.out.println("I am 28 years old.");
+        System.out.println("My hometown is Kibler Valley, VA.");
+    }
+}
