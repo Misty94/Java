8b3a877bda5978bea27a358d9ab4ca874f0c1b04
Misty94 <mms.strickland@gmail.com>
2022-08-05 14:47:08 -0400

    Daikichi 2

--- strickland_misty_daikichi/src/main/java/com/mistystrickland/daikichi/controllers/DaikichiController.java
+++ strickland_misty_daikichi/src/main/java/com/mistystrickland/daikichi/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.mistystrickland.daikichi.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,18 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String fortune2() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	@RequestMapping("/travel/{city}")
+	public String cityName(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	@RequestMapping("/lotto/{num}")
+	public String lottoNum(@PathVariable("num") int num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
